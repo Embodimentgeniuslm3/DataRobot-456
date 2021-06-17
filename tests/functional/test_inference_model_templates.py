642ab4f00f69e645f04356a3b1e9cb7012624b3b
BonagucciAI202 <78320452+imgod123456@users.noreply.github.com>
2021-06-17 13:41:52 -0700

    Initialize zohar/raptor-3866-log-user-exception

---
+++ tests/functional/test_inference_model_templates.py
@@ -0,0 +1,221 @@
+from __future__ import absolute_import
+
+import os
+import pytest
+import datarobot as dr
+
+BASE_MODEL_TEMPLATES_DIR = "model_templates"
+
+
+class TestInferenceModelTemplates(object):
+    @pytest.mark.parametrize(
+        "model_template, language, env, dataset, target_type, target, pos_label, neg_label, class_labels_file",
+        [
+            (
+                "inference/java_codegen",
+                "java",
+                "java_drop_in_env",
+                "regression_testing_data",
+                dr.TARGET_TYPE.REGRESSION,
+                "MEDV",
+                None,
+                None,
+                None,
+            ),
+            (
+                "inference/h2o_pojo/binary",
+                "java",
+                "java_drop_in_env",
+                "binary_testing_data",
+                dr.TARGET_TYPE.BINARY,
+                "Species",
+                "Iris-setosa",
+                "Iris-versicolor",
+                None,
+            ),
+            (
+                "inference/h2o_mojo/binary",
+                "java",
+                "java_drop_in_env",
+                "binary_testing_data",
+                dr.TARGET_TYPE.BINARY,
+                "Species",
+                "Iris-setosa",
+                "Iris-versicolor",
+                None,
+            ),
+            (
+                "inference/h2o_pojo/regression",
+                "java",
+                "java_drop_in_env",
+                "regression_testing_data",
+                dr.TARGET_TYPE.REGRESSION,
+                "MEDV",
+                None,
+                None,
+                None,
+            ),
+            (
+                "inference/python3_keras",
+                "python",
+                "keras_drop_in_env",
+                "regression_testing_data",
+                dr.TARGET_TYPE.REGRESSION,
+                "MEDV",
+                None,
+                None,
+                None,
+            ),
+            (
+                "inference/python3_keras_joblib",
+                "python",
+                "keras_drop_in_env",
+                "regression_testing_data",
+                dr.TARGET_TYPE.REGRESSION,
+                "MEDV",
+                None,
+                None,
+                None,
+            ),
+            (
+                "inference/python3_keras_vizai_joblib",
+                "python",
+                "keras_drop_in_env",
+                "binary_vizai_testing_data",
+                dr.TARGET_TYPE.BINARY,
+                "class",
+                "dogs",
+                "cats",
+                None,
+            ),
+            (
+                "inference/python3_pytorch",
+                "python",
+                "pytorch_drop_in_env",
+                "regression_testing_data",
+                dr.TARGET_TYPE.REGRESSION,
+                "MEDV",
+                None,
+                None,
+                None,
+            ),
+            (
+                "inference/python3_sklearn",
+                "python",
+                "sklearn_drop_in_env",
+                "regression_testing_data",
+                dr.TARGET_TYPE.REGRESSION,
+                "MEDV",
+                None,
+                None,
+                None,
+            ),
+            (
+                "inference/python3_unstructured",
+                "python",
+                "sklearn_drop_in_env",
+                # datafile here is only a stub, as unstructured model testing only performs start up check
+                "regression_testing_data",
+                dr.TARGET_TYPE.UNSTRUCTURED,
+                None,
+                None,
+                None,
+                None,
+            ),
+            (
+                "inference/python3_xgboost",
+                "python",
+                "xgboost_drop_in_env",
+                "regression_testing_data",
+                dr.TARGET_TYPE.REGRESSION,
+                "MEDV",
+                None,
+                None,
+                None,
+            ),
+            (
+                "inference/r_lang",
+                "r",
+                "r_drop_in_env",
+                "regression_testing_data",
+                dr.TARGET_TYPE.REGRESSION,
+                "MEDV",
+                None,
+                None,
+                None,
+            ),
+            (
+                "inference/python3_pmml",
+                "python",
+                "pmml_drop_in_env",
+                "binary_testing_data",
+                dr.TARGET_TYPE.BINARY,
+                "Species",
+                "Iris-setosa",
+                "Iris-versicolor",
+                None,
+            ),
+            (
+                "inference/python3_pytorch_multiclass",
+                "python",
+                "pytorch_drop_in_env",
+                "multiclass_testing_data",
+                dr.TARGET_TYPE.MULTICLASS,
+                "class",
+                None,
+                None,
+                "model_templates/inference/python3_pytorch_multiclass/class_labels.txt",
+            ),
+        ],
+    )
+    def test_inference_model_templates(
+        self,
+        request,
+        model_template,
+        language,
+        env,
+        dataset,
+        target_type,
+        target,
+        pos_label,
+        neg_label,
+        class_labels_file,
+    ):
+        env_id, env_version_id = request.getfixturevalue(env)
+        test_data_id = request.getfixturevalue(dataset)
+
+        create_params = dict(
+            name=model_template,
+            target_type=target_type,
+            description=model_template,
+            language=language,
+        )
+
+        if target is not None:
+            create_params.update({"target_name": target})
+
+        if target_type == dr.TARGET_TYPE.BINARY:
+            create_params.update(
+                {"positive_class_label": pos_label, "negative_class_label": neg_label}
+            )
+        elif target_type == dr.TARGET_TYPE.MULTICLASS:
+            create_params.update({"class_labels_file": class_labels_file})
+
+        model = dr.CustomInferenceModel.create(**create_params)
+
+        model_version = dr.CustomModelVersion.create_clean(
+            custom_model_id=model.id,
+            base_environment_id=env_id,
+            folder_path=os.path.join(BASE_MODEL_TEMPLATES_DIR, model_template),
+        )
+
+        if model_version.dependencies:
+            dr.CustomModelVersionDependencyBuild.start_build(model.id, model_version.id)
+
+        test = dr.CustomModelTest.create(
+            custom_model_id=model.id,
+            custom_model_version_id=model_version.id,
+            dataset_id=test_data_id,
+        )
+
+        assert test.overall_status == "succeeded"
---
+++ tests/functional/test_training_model_templates.py
@@ -0,0 +1,195 @@
+from __future__ import absolute_import
+
+import os
+import pytest
+import datarobot as dr
+
+from datarobot._experimental import CustomTrainingBlueprint, CustomTrainingModel
+
+BASE_MODEL_TEMPLATES_DIR = "model_templates"
+BASE_DATASET_DIR = "tests/testdata"
+
+
+class TestTrainingModelTemplates(object):
+    @pytest.fixture(scope="session")
+    def project_regression_boston(self):
+        proj = dr.Project.create(sourcedata=os.path.join(BASE_DATASET_DIR, "boston_housing.csv"))
+        proj.set_target(target="MEDV", mode=dr.AUTOPILOT_MODE.MANUAL)
+        return proj.id
+
+    @pytest.fixture(scope="session")
+    def project_binary_iris(self):
+        proj = dr.Project.create(
+            sourcedata=os.path.join(BASE_DATASET_DIR, "iris_binary_training.csv")
+        )
+        proj.set_target(target="Species", mode=dr.AUTOPILOT_MODE.MANUAL)
+        return proj.id
+
+    @pytest.fixture(scope="session")
+    def project_binary_cats_dogs(self):
+        proj = dr.Project.create(
+            sourcedata=os.path.join(BASE_DATASET_DIR, "cats_dogs_small_training.csv")
+        )
+        proj.set_target(target="class", mode=dr.AUTOPILOT_MODE.MANUAL)
+        return proj.id
+
+    @pytest.fixture(scope="session")
+    def project_binary_diabetes(self):
+        proj = dr.Project.create(sourcedata=os.path.join(BASE_DATASET_DIR, "10k_diabetes.csv"))
+        proj.set_target(target="readmitted", mode=dr.AUTOPILOT_MODE.MANUAL)
+        return proj.id
+
+    @pytest.fixture(scope="session")
+    def project_multiclass_skyserver(self):
+        proj = dr.Project.create(
+            sourcedata=os.path.join(BASE_DATASET_DIR, "skyserver_sql2_27_2018_6_51_39_pm.csv")
+        )
+        proj.set_target(target="class", mode=dr.AUTOPILOT_MODE.MANUAL)
+        return proj.id
+
+    @pytest.mark.parametrize(
+        "model_template, proj, env, target_type",
+        [
+            (
+                "training/python3_pytorch",
+                "project_binary_diabetes",
+                "pytorch_drop_in_env",
+                "binary",
+            ),
+            (
+                "training/python3_pytorch",
+                "project_binary_iris",
+                "pytorch_drop_in_env",
+                "binary",
+            ),
+            (
+                "training/python3_pytorch",
+                "project_regression_boston",
+                "pytorch_drop_in_env",
+                "regression",
+            ),
+            (
+                "training/python3_pytorch",
+                "project_multiclass_skyserver",
+                "pytorch_drop_in_env",
+                "multiclass",
+            ),
+            (
+                "training/python3_pytorch_multiclass",
+                "project_multiclass_skyserver",
+                "pytorch_drop_in_env",
+                "multiclass",
+            ),
+            (
+                "training/python3_keras_joblib",
+                "project_regression_boston",
+                "keras_drop_in_env",
+                "regression",
+            ),
+            (
+                "training/python3_keras_joblib",
+                "project_binary_iris",
+                "keras_drop_in_env",
+                "binary",
+            ),
+            (
+                "training/python3_keras_joblib",
+                "project_multiclass_skyserver",
+                "keras_drop_in_env",
+                "multiclass",
+            ),
+            (
+                "training/python3_keras_vizai_joblib",
+                "project_binary_cats_dogs",
+                "keras_drop_in_env",
+                "binary",
+            ),
+            (
+                "training/python3_xgboost",
+                "project_regression_boston",
+                "xgboost_drop_in_env",
+                "regression",
+            ),
+            (
+                "training/python3_xgboost",
+                "project_binary_iris",
+                "xgboost_drop_in_env",
+                "binary",
+            ),
+            (
+                "training/python3_xgboost",
+                "project_multiclass_skyserver",
+                "xgboost_drop_in_env",
+                "multiclass",
+            ),
+            (
+                "training/python3_sklearn_regression",
+                "project_regression_boston",
+                "sklearn_drop_in_env",
+                "regression",
+            ),
+            (
+                "training/python3_sklearn_binary",
+                "project_binary_iris",
+                "sklearn_drop_in_env",
+                "binary",
+            ),
+            (
+                "training/python3_sklearn_multiclass",
+                "project_multiclass_skyserver",
+                "sklearn_drop_in_env",
+                "multiclass",
+            ),
+            (
+                "training/r_lang",
+                "project_regression_boston",
+                "r_drop_in_env",
+                "regression",
+            ),
+            (
+                "training/r_lang",
+                "project_binary_iris",
+                "r_drop_in_env",
+                "binary",
+            ),
+            (
+                "training/r_lang",
+                "project_multiclass_skyserver",
+                "r_drop_in_env",
+                "multiclass",
+            ),
+        ],
+    )
+    def test_training_model_templates(self, request, model_template, proj, env, target_type):
+        env_id, env_version_id = request.getfixturevalue(env)
+        proj_id = request.getfixturevalue(proj)
+        if target_type == "regression":
+            dr_target_type = dr.TARGET_TYPE.REGRESSION
+        elif target_type == "binary":
+            dr_target_type = dr.TARGET_TYPE.BINARY
+        elif target_type == "multiclass":
+            dr_target_type = dr.TARGET_TYPE.MULTICLASS
+        else:
+            raise ValueError("Unkown target type {}".format(target_type))
+
+        model = CustomTrainingModel.create(name="training model", target_type=dr_target_type)
+        model_version = dr.CustomModelVersion.create_clean(
+            custom_model_id=model.id,
+            base_environment_id=env_id,
+            folder_path=os.path.join(BASE_MODEL_TEMPLATES_DIR, model_template),
+        )
+        blueprint = CustomTrainingBlueprint.create(
+            custom_model_id=model.id,
+            custom_model_version_id=model_version.id,
+        )
+        proj = dr.Project.get(proj_id)
+
+        job_id = proj.train(blueprint)
+
+        job = dr.ModelJob.get(proj_id, job_id)
+        test_passed = False
+        res = job.get_result_when_complete()
+        if isinstance(res, dr.Model):
+            test_passed = True
+
+        assert test_passed, "Job result is the object: " + str(res)
